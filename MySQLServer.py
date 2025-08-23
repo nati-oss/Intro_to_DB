afdad97cb6ead0b0cb6ef7b1d7022b674fdcf405
xilak <natanzerihun101@gmail.com>
2025-08-23 11:20:31 +0300

    after a week

--- MySQLServer.py
+++ MySQLServer.py
@@ -17,7 +17,7 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
     
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error: Could not connect to the database server. {e}")
     
     finally:
